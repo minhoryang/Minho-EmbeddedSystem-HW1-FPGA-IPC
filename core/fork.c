2256b469b655d91cedc5fa2035e610851edb1db4
minhoryang <minhoryang@gmail.com>
2014-04-15 22:32:52 +0900

    Little comment @ fork.c

--- core/fork.c
+++ core/fork.c
@@ -19,7 +19,13 @@
 #endif
 
 pid_t new_fork(int (*child)(void)){
-	// TODO SIGNAL.
+	// TODO discuss SIGNAL.
+	// XXX As far as I tested,
+	//    1) if parent exit safely, parent didn't wait childs.
+	//    2) if parent exit unexpectedly, parent kill childs also.
+	// need to 
+	//    1) parent say goodbye to childs.
+	//    2) chld say goodbay to parent.
 	pid_t ret = fork();
 	switch(ret){
 		case -1:
